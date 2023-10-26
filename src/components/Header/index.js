b64e9ef66be432a58155eabab10df50c4b3d0808
Itallo Vasconcelos <itallo.vasconcelos@versatushpc.com.br>
2023-10-26 01:02:26 -0300

    Creating pages and routes of pages.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { Nav } from './styled';
 
 export default function Header() {
-  // eslint-disable-next-line no-unused-vars, no-console
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
-
   return (
     <Nav>
       <Link to="/">
         <FaHome size="24px" />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaSignInAlt size="24px" />
       </Link>
-      <Link to="/logout">
+      <Link to="/login">
         <FaUserAlt size="24px" />
       </Link>
-      {botaoClicado ? 'Clicado' : 'Não clicado'}
     </Nav>
   );
 }
