be255c90ba6f6ea75aee7f51ea5248d07fdfa15f
Itallo Vasconcelos <itallo.vasconcelos@versatushpc.com.br>
2023-10-31 12:47:43 -0300

    Editing and adding new user photo

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,11 +1,74 @@
-import React from 'react';
-
+import { get } from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import Loading from '../../components/Loading';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Fotos() {
+export default function Fotos({ match }) {
+  const id = get(match, 'params.id', '');
+  const [isLoading, setIsLoading] = useState(false);
+  const [foto, setFoto] = useState('');
+  const dispatch = useDispatch();
+  const handleSaveImg = async (e) => {
+    const fotoArquivo = e.target.files[0];
+    const fotoURL = URL.createObjectURL(fotoArquivo);
+    setFoto(fotoURL);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('photo', fotoArquivo);
+    try {
+      setIsLoading(true);
+      await axios.post('/photos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Foto enviada com sucesso!');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const { status } = get(err, 'response', '');
+      toast.error('Erro ao enviar foto!');
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        setFoto(get(data, 'Photos[0].photoUrl', ''));
+        setIsLoading(false);
+      } catch (err) {
+        toast.error('Erro ao obter imagem');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+    getData();
+  }, [id]);
   return (
     <Container>
-      <h1>Fotos Page</h1>
+      <Loading isLoading={isLoading} />
+      <Title>Fotos Page</Title>
+      <Form>
+        <label htmlFor="foto">
+          {foto ? <img src={foto} alt="Foto" /> : 'Selecionar'}
+          <input type="file" id="foto" onChange={handleSaveImg} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Fotos/styled.js
+++ src/pages/Fotos/styled.js
@@ -1,5 +1,62 @@
 import styled from 'styled-components';
 
+import * as colors from '../../config/colors';
+
 export const Title = styled.h1`
-  background: red;
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    background: #eee;
+    border: 5px dashed ${colors.primaryColor};
+    border-radius: 50%;
+    margin: 30px auto;
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
+  input {
+    display: none;
+  }
+  img {
+    width: 180px;
+    height: 180px;
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px;
+  position: relative;
+  margin-top: 20px;
+
+  img {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    position: absolute;
+    bottom: 0;
+    color: #fff;
+    background: ${colors.primaryColor};
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
 `;
